c524beec6fa1c69976d68d9ef35278f28df4b186
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-03-09 12:43:36 +0530

    Update RangeandFrequency.c

--- RangeandFrequency.c
+++ RangeandFrequency.c
@@ -5,7 +5,7 @@ int TotalNoOfSamples=0;
 int CurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
 int TotalSamples=sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
   
-int ReadNoofSamples(int *CurrentSamples , int TotalSamples ,int FromRange , int ToRange)
+int ReadNoofSamples(int FromRange , int ToRange)
 {
   int NoofOccurance=0;
   for(int i=0;i<TotalSamples;i++)
