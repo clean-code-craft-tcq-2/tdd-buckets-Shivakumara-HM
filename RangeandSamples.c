72b0c8a47efca67a7b66db9d773f66b2a1bcbfc8
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-03-09 07:57:55 +0530

    Update RangeandSamples.c

--- RangeandSamples.c
+++ RangeandSamples.c
@@ -6,6 +6,29 @@ currentrange getrange(int *Samples)
   currentrange range;
   range.min = findminvalue;
   range.max = findmaxvalue;
-  range.count = getcountofcontinousrange;
+  range.count = getfrequency(Samples);
   return range;
 }
+
+
+int getfrequency(int *Samples)
+{
+  int lengthofinput;
+  int comp = 1;
+  int frequency;
+  lengthofinput = sizeof(Samples) / sizeof(Samples[0];
+  for(i=0; i<= lengthofinput ;i++)
+  {
+     comp = Samples[i+1] - CurrentSamples[i];
+     if((comp == 0) || (comp == 1) || (comp==2))
+        {
+          frequency++;
+        }
+        else
+        {
+          frequency = 0;
+        }
+    }
+  return frequency;
+}
+
