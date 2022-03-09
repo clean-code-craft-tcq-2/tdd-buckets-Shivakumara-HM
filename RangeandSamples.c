129cd54c615b40d540e533d4280c6453f9ae80a1
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-03-09 07:45:26 +0530

    Update RangeandSamples.c

--- RangeandSamples.c
+++ RangeandSamples.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include "RangeandSamples.h"
 
+currentrange getrange(int *Samples)
+{
+  currentrange range;
+  range.min = findminvalue;
+  range.max = findmaxvalue;
+  range.count = getcountofcontinousrange;
+  return range;
+}
