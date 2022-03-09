4130deb4e645e4d633fc4a210baf3f5a223325d2
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-03-09 08:22:09 +0530

    Update RangeandFrequency.h

--- RangeandFrequency.h
+++ RangeandFrequency.h
@@ -3,7 +3,7 @@ typedef struct
   int min;
   int max;
   int count;
-} currentrange;
+} rangeandfrequency;
 
-currentrange getrange(int *Samples);
+rangeandfrequency getrange(int *Samples);
 int getfrequency(int *Samples); 
