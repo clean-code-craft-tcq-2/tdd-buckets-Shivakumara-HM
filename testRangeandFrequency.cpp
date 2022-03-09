8abe0a954c2f20821af6313ca9e19ebbf1a42e94
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-03-09 12:49:38 +0530

    Update testRangeandFrequency.cpp

--- testRangeandFrequency.cpp
+++ testRangeandFrequency.cpp
@@ -6,24 +6,15 @@
 
 int NoOfSamples=0;
 
-
-/*Reading occurance betweeen 5-10 range */
-TEST_CASE("Read No of samples from 5-10 range")
+TEST_CASE("Read No of samples from 4-5 range")
 {
- NoOfSamples=ReadNoofSamples(5,10);
+ NoOfSamples=ReadNoofSamples(4,5);
  REQUIRE(NoOfSamples == 2);
 }
 
-/*Reading occurance betweeen 3-5 range */
-TEST_CASE("Read No of samples from 3-5 range")
+TEST_CASE("Read No of samples from 10-12 range")
 {
- NoOfSamples=ReadNoofSamples(3,5);
- REQUIRE(NoOfSamples == 4);
+ NoOfSamples=ReadNoofSamples(10,12);
+ REQUIRE(NoOfSamples == 3);
 }
 
-/*Reading occurance betweeen 3-12 range */
-TEST_CASE("Read No of samples from 3-12 range")
-{
- NoOfSamples=ReadNoofSamples(3,12);
- REQUIRE(NoOfSamples == 7);
-}
