5c27cd191a0fa71a86cde17600f554d498ba3d98
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-03-16 12:40:20 +0530

    Update RangeandFrequency.h

--- RangeandFrequency.h
+++ RangeandFrequency.h
@@ -1,7 +1,7 @@
 int ReadNoOfSamples(int *CurrentSamples, int NoOfSamples);
 int* SortArray(int *CurrentSamples, int NoOfSamples);
 int CompareInputs (const void * firstInput, const void * secondInput);
-int SamplesRangeCount(int* CurrentSamples, int NoOfSamples);
+int SamplesRangeCount(int* SortedCurrentSamples, int NoOfSamples);
 void PrintonConsole(char * InputData);
 void SendDataToPrint(int FromRange , int ToRange ,int TotalOccurance);
 
