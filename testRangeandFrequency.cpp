0c78c998311e71b284f35a8576b238cefb057102
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-03-16 12:56:20 +0530

    Update testRangeandFrequency.cpp

--- testRangeandFrequency.cpp
+++ testRangeandFrequency.cpp
@@ -7,14 +7,14 @@
 int CurrentSamples1[4] = {3, 3, 5, 4};
 int CurrentSamples2[3] = {10,11,12};
 
-TEST_CASE("Read No of samples from range")
+TEST_CASE("Read No of samples from range 3 to 5")
 {
  int Samples = sizeof(CurrentSamples1)/sizeof(CurrentSamples1[0]);
  int TotalNoOfSamples = ReadNoOfSamples(CurrentSamples1, Samples);
  REQUIRE(TotalNoOfSamples == 2);
 }
 
-TEST_CASE("Read No of samples from range")
+TEST_CASE("Read No of samples from range 10 to 12")
 {
  int Samples = sizeof(CurrentSamples2)/sizeof(CurrentSamples2[0]);
  int TotalNoOfSamples = ReadNoOfSamples(CurrentSamples2, Samples);
