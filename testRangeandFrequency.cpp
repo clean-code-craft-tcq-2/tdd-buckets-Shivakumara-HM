d513048fb20c9eb117adce1e1bfddbd2a7acdca6
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-03-31 11:14:06 +0530

    Update testRangeandFrequency.cpp

--- testRangeandFrequency.cpp
+++ testRangeandFrequency.cpp
@@ -1,12 +1,12 @@
 #define CATCH_CONFIG_MAIN
 
-#include "assert.h"
 #include "catch.hpp"
 #include "RangeandFrequency.h"
 
 int CurrentSamples1[4] = {3, 3, 5, 4};
 int CurrentSamples2[3] = {10,11,12};
 int CurrentSamples3[7] = {-3, 3 ,-5, -4, -3, 11, 12};
+int CurrentAnalogSamples[12] ={1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12};
 
 TEST_CASE("Read No of samples from range 3 to 5")
 {
@@ -27,6 +27,13 @@ TEST_CASE("Read No of samples from range 3 to 12")
  int Samples = sizeof(CurrentSamples3)/sizeof(CurrentSamples3[0]);
  int TotalNoOfSamples = ReadNoOfSamples(CurrentSamples3, Samples);
  REQUIRE(TotalNoOfSamples == 7);
+}
+
+TEST_CASE("Read No of sequence samples from an array of 12 consecutive samples")
+{
+ int Samples = sizeof(CurrentAnalogSamples)/sizeof(CurrentAnalogSamples[0]);
+ int TotalNoOfSamples=ReadNoOfSamples(CurrentAnalogSamples, 12);
+ REQUIRE(TotalNoOfSamples == 1);
 }
 
 
