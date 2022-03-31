da3f58e44a4518c12aeda7d1d64a8a587db24357
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-03-31 10:58:47 +0530

    Update RangeandFrequency.h

--- RangeandFrequency.h
+++ RangeandFrequency.h
@@ -1,4 +1,5 @@
-int ReadNoOfSamples(int *CurrentSamples, int NoOfSamples);
+int ReadNoOfSamples(int *Readings, int NoOfSamples);
+int* ConvertA2D(int *Readings, int NoOfSamples);
 int* SortArray(int *CurrentSamples, int NoOfSamples);
 int CompareInputs (const void * firstInput, const void * secondInput);
 int SamplesRangeCount(int* SortedCurrentSamples, int NoOfSamples);
